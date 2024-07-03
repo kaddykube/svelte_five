493d5df2c04c856836356b0368c25ec0922d3cd2
kaddykube <148755325+kaddykube@users.noreply.github.com>
2024-07-03 13:53:53 +0200

    Update today-tasks.svelte

--- app/src/lib/components/sections/today-tasks.svelte
+++ app/src/lib/components/sections/today-tasks.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
   import { createList, type Item } from "$lib/store/todos.svelte";
   // wird nicht geupdated, also bisher nicht reactive
-  const list = createList();
+  const storelist = $derived(createList());
 </script>
 
 <div class=" pt-[50px] w-1/3 font-libre">
   <h2 class="text-[20px] p-2">heute zu erledigen</h2>
   <ol>
-    {#if list}
-      {#each list.list as li}
+    {#if storelist.list}
+      {#each storelist.list as li}
         <li>{li.text}</li>
       {/each}
     {/if}
