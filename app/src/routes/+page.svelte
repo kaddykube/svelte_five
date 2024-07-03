9a34da4e2d4c82a251a4c513f96f5805d9cab632
kaddykube <148755325+kaddykube@users.noreply.github.com>
2024-07-03 09:39:58 +0200

    setup

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-[100vw] h-[100vh] flex items-center">
+  <div class="max-w-[2000px] w-full h-full">
+    <div class="flex w-full h-full flex-col items-center">
+      <h1 class="text-lime-500">Willkommen!</h1>
+    </div>
+  </div>
+</div>
